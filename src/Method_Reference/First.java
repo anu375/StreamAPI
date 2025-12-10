b487e195b1f885add9d2738422f66adbd223f74f
Anushka Jadhav <anujadhav32004@gmail.com>
2025-12-10 10:56:46 +0530

    first commit

---
+++ src/Method_Reference/First.java
@@ -0,0 +1,43 @@
+package Method_Reference;
+
+import java.util.Comparator;
+import java.util.TreeSet;
+
+public class First
+{
+
+	public static void main(String[] args) 
+	{
+		TreeSet<Integer> tree = new TreeSet<Integer>((d1,d2)->{return Integer.compare(d2,d1);});
+		
+		tree.add(23);
+		tree.add(24);
+		tree.add(234);
+		tree.add(34);
+		tree.add(3);
+		
+		System.out.println(tree);
+	}
+
+}
+
+class IntegerComparator implements Comparator<Integer>
+{
+	public int compare(Integer i1, Integer i2)
+	{
+		return Integer.compare(i1, i2);
+	}
+	
+	public boolean equals(Object obj)
+	{
+		return true;
+	}
+}
+
+class IntegerComparator2 implements Comparator<Integer>
+{
+	public int compare(Integer o1,Integer o2)
+	{
+		return Integer.compare(o1, o2);
+	}
+}
